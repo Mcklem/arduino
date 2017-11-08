374c9797e7d3c8a38a743d4aa369d0a7723ff812
mcklem <jesus_2394@hotmail.com>
2017-11-08 17:29:02 +0100

    Updated access point.

--- examples/NodeMCU/ESP8266_AccessPoint/ESP8266_AccessPoint.ino
+++ examples/NodeMCU/ESP8266_AccessPoint/ESP8266_AccessPoint.ino
@@ -8,12 +8,45 @@
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 
-const char *ssid = "ABC";
+const char *ssid = "IM_NOT_AN_ACCESS_POINT";
 const char *password = "12345678";
 int stateLED = LOW;
+bool blinkState = false;
 #define led_pin 16
 
 ESP8266WebServer server(80);
+
+const String HtmlHtml = "<html><head>"
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /></head>";
+const String HtmlHtmlClose = "</html>";
+const String HtmlTitle = "<h1>Arduino-er: ESP8266 AP WebServer exercise</h1><br/>\n";
+const String HtmlLedStateLow = "<big>LED is now <b>ON</b></big><br/>\n";
+const String HtmlLedStateHigh = "<big>LED is now <b>OFF</b></big><br/>\n";
+const String HtmlButtons = 
+    "<a href=\"LEDOn\"><button style=\"display: block; width: 100%;\">ON</button></a><br/>"
+    "<a href=\"LEDOff\"><button style=\"display: block; width: 100%;\">OFF</button></a><br/>"
+    "<a href=\"Hello\"><button style=\"display: block; width: 100%;\">HELLO</button></a><br/>";
+
+void setup() {
+    delay(1000);
+    Serial.begin(115200);
+    Serial.println();
+
+    WiFi.softAP(ssid, password);
+
+    IPAddress apip = WiFi.softAPIP();
+    Serial.print("visit: \n");
+    Serial.println(apip);
+    server.on("/", handleRoot);
+    server.on("/LEDOn", handleLedOn);
+    server.on("/LEDOff", handleLedOff);
+    server.on("/Hello", handleHello);
+    server.on("/Time", handleTime);
+    server.begin();
+    Serial.println("HTTP server beginned");
+    pinMode(led_pin, OUTPUT);
+    digitalWrite(led_pin, stateLED);
+}
 
 void handleRoot() {
     response();
@@ -31,18 +64,23 @@ void handleLedOff() {
   response();
 }
 
-const String HtmlHtml = "<html><head>"
-    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /></head>";
-const String HtmlHtmlClose = "</html>";
-const String HtmlTitle = "<h1>Arduino-er: ESP8266 AP WebServer exercise</h1><br/>\n";
-const String HtmlLedStateLow = "<big>LED is now <b>ON</b></big><br/>\n";
-const String HtmlLedStateHigh = "<big>LED is now <b>OFF</b></big><br/>\n";
-const String HtmlButtons = 
-    "<a href=\"LEDOn\"><button style=\"display: block; width: 100%;\">ON</button></a><br/>"
-    "<a href=\"LEDOff\"><button style=\"display: block; width: 100%;\">OFF</button></a><br/>";
+void handleHello() {
+  Serial.println("Hello darkness my old friend");
+  response();
+}
+
+void handleTime() {
+  Serial.println("Hello darkness my old friend");
+  String htmlRes = HtmlHtml;
+  int t = millis()/1000;
+  htmlRes += String(t)  + "s";
+  htmlRes += HtmlHtmlClose;
+  server.send(200, "text/html", htmlRes);
+}
 
 void response(){
   String htmlRes = HtmlHtml + HtmlTitle;
+
   if(stateLED == LOW){
     htmlRes += HtmlLedStateLow;
   }else{
@@ -55,24 +93,6 @@ void response(){
   server.send(200, "text/html", htmlRes);
 }
 
-void setup() {
-    delay(1000);
-    Serial.begin(115200);
-    Serial.println();
-
-    WiFi.softAP(ssid, password);
-
-    IPAddress apip = WiFi.softAPIP();
-    Serial.print("visit: \n");
-    Serial.println(apip);
-    server.on("/", handleRoot);
-    server.on("/LEDOn", handleLedOn);
-    server.on("/LEDOff", handleLedOff);
-    server.begin();
-    Serial.println("HTTP server beginned");
-    pinMode(led_pin, OUTPUT);
-    digitalWrite(led_pin, stateLED);
-}
 
 void loop() {
     server.handleClient();
